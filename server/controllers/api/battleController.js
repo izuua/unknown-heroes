f2509c1120a43e84f066d75a640dfee1dda72185
izuua <izuuaaf@gmail.com>
2019-09-06 01:17:47 -0400

    Added server logic to do combat operations.

--- server/controllers/api/battleController.js
+++ server/controllers/api/battleController.js
@@ -1,11 +1,28 @@
 const battleController = require('express').Router();
 const battlefunctions = require('../../logic/battlefunctions');
+// const app = require('../../server.js')
 
 
 battleController.post("/attack", (req, res) => {
     console.log(req.body);
-    num = battlefunctions.random(1, 5);
-    res.send({ num: num})
+
+    let messageP = battlefunctions.attack(req.body.hero, req.body.monster);
+    let messageE = battlefunctions.attack(req.body.monster, req.body.hero);
+    let gameOver = false;
+
+    // console.log(req.body.hero);
+    // console.log(req.body.monster);
+    console.log(req.body.num);
+    received = true;
+
+    res.send({ 
+        messageP: messageP,
+        messageE: messageE,
+        gameOver: gameOver,
+        received: received
+    })
 })
+
+
 
 module.exports = battleController;
--- server/logic/battlefunctions.js
+++ server/logic/battlefunctions.js
@@ -1,8 +1,35 @@
 battlefunctions = {
     random(min, max) {
         return Math.floor((Math.random() * max) + min)
+    },
+
+    accuracy(object1, object2) {
+        let hitRate = 75 + ((object1.eva - object2.eva)*3);
+        let accCheck = battlefunctions.random(1, 100);
+        if (accCheck <= hitRate) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    attack(attacker, defender) {
+        accCheck = battlefunctions.accuracy(attacker, defender)
+        if (accCheck === true) {
+            damage = (battlefunctions.random(1,5) + (attacker.atk - defender.def));
+            return `The ${attacker.name} hits the ${req.body.defender.name} for ${damage} damage.`
+        } else {
+            return `The ${attacker.name} misses the ${defender.name}.`
+        }
+        
+    },
+
+    gameOverCheck() {
+
     }
 }
+
+
 
 module.exports = battlefunctions;
 
