37eade7f5f77dbd7d47151a04152431897dfbe2a
Caleb Myers <cmyers5108@gmail.com>
2019-09-06 13:40:31 -0400

    fixed btn issues

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./home.css";
-// import logo from './logo.svg';
+import AuthContext from '../../contexts/AuthContext';
 
 class HomePage extends Component {
+  static contextType = AuthContext
+
   render() {
     return (
       <div id="homepage">
-            <h1 id="gametitle"><strong>Unknown Heros</strong></h1>
-            
-            <button id="login"><Link to="/login">Log In</Link></button>
-    
-            <button id="createaccount"><Link to="/register">Create Account</Link></button>
-        </div>
+        <h1 id="gametitle"><strong>Unknown Heros</strong></h1>
+
+        {this.context.user ? (
+          <Link to="/character"><button id="login">Start Game</button></Link >
+        ) : (
+            <div>
+              <Link to="/login"><button id="login">Log In</button></Link>
+              <Link to="/register"><button id="createaccount">Create Account</button></Link>
+            </div>
+          )
+        }
+      </div>
     );
   }
 }
