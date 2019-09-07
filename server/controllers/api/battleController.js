69952d1f635f0ee8ca0673fcd73c6bb6e0ced812
izuua <izuuaaf@gmail.com>
2019-09-06 21:31:32 -0400

    Added logic to set gameOver to true

--- server/controllers/api/battleController.js
+++ server/controllers/api/battleController.js
@@ -10,44 +10,57 @@ battleController.post("/start", (req, res) => {
     enemy.hp = enemy.maxHp;
     console.log(player);
     console.log(enemy);
+
+    received = true;
+
+    res.send({
+        received: received
+    })
 })
 
 
 battleController.get("/attack", (req, res) => {
-    console.log(req.body);
+    let gameOver = false;
+    playerDead = "";
 
     playerAccCheck = battlefunctions.accuracy(player, enemy);
     enemyAccCheck = battlefunctions.accuracy(player, enemy);
     if (playerAccCheck) {
-        damage =battlefunctions.attack(player, enemy);
-        enemy.hp = enemy.hp-damage;
-        playerMessage = `${player.name} hit target for ${damage}`
+        damage = battlefunctions.attack(player, enemy);
+        enemy.hp = enemy.hp - damage;
+        playerMessage = `${player.name} hit ${enemy.name} for ${damage} damage.`
     } else {
-
-        playerMessage = `${player.name} misses ${enemy.name}`
+        playerMessage = `${player.name} misses ${enemy.name}.`
     }
 
-    if (enemyAccCheck) {
-        damage = battlefunctions.attack(enemy, player);
-        player.hp = player.hp-damage;
-        enemyMessage = `${enemy.name} hit taget for ${damage}`
+    if (enemy.hp > 0) {
+        if (enemyAccCheck) {
+            damage = battlefunctions.attack(enemy, player);
+            player.hp = player.hp - damage;
+            enemyMessage = `${enemy.name} hit ${player.name} for ${damage} damage.`
+        } else {
+            enemyMessage = `${enemy.name} misses ${player.name}.`
+        }
     } else {
-enemyMessage = `${enemy.name} misses ${player.name}`
+        enemyMessage = `${enemy.name} is knocked out!`;
+        gameOver = true;
     }
 
-
-    let gameOver = false;
+    if (player.hp <= 0) {
+        gameOver = true;
+        playerDead = `${player.name} is knocked out!`;
+    }
 
     received = true;
 
-    res.send({ 
+    res.send({
+        playerHp: player.hp,
+        enemyHp: enemy.hp,
         playerMessage: playerMessage,
         enemyMessage: enemyMessage,
+        playerDead: playerDead,
         gameOver: gameOver,
-        received: received,
-        playerHp: player.hp,
-        enemyHp: enemy.hp 
-
+        received: received
     })
 })
 
